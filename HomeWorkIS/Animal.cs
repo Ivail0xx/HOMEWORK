114ad0e45c7aa8c2daa2500aa77e67460e4a579b
Ivaylo Stefanov <113666488+Ivail0xx@users.noreply.github.com>
2022-12-01 22:08:18 +0200

    Add files via upload

---
+++ HomeWorkIS/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkIS
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public int AnimalTypeId { get; set; }
+        public AnimalType AnimalTypes { get; set; }
+    }
+}
---
+++ HomeWorkIS/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkIS
+{
+    public class Context : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<AnimalType> Types { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) =>
+            builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Animal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
